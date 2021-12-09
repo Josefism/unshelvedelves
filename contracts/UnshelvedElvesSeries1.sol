44edc8259c1d4d5b115dba6a66c24a97ec1fc1f8
Josef Cook <josef@assemblystudio.com>
2021-12-09 11:04:02 -0600

    Added Series 1 inidication. Prepping for deploy.

--- contracts/UnshelvedElvesSeries1.sol
+++ contracts/UnshelvedElvesSeries1.sol
@@ -4,20 +4,21 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-contract UnshelvedElves is ERC721Enumerable, Ownable {
+contract UnshelvedElvesSeries1 is ERC721Enumerable, Ownable {
     using SafeMath for uint256;
     using Counters for Counters.Counter;
     
     Counters.Counter private _tokenIds;
 
-    uint public constant MAX_SUPPLY = 25000;
-    uint public constant PRICE = 0.01 ether;
+    uint public constant MAX_SUPPLY = 20000;
+    uint public constant PRICE = 10 ether;
     uint public constant MAX_PER_MINT = 5;
     
     string public baseTokenURI;
 
-    constructor(string memory baseURI) ERC721("Unshelved Elves", "UE") {
+    constructor(string memory baseURI) ERC721("Unshelved Elves (Series 1)", "UE1") {
         setBaseURI(baseURI);
+        _tokenIds.increment();
     }
 
     function reserveNFTs(uint _count) public onlyOwner {
@@ -38,9 +39,19 @@ contract UnshelvedElves is ERC721Enumerable, Ownable {
 
     function mintNFTs(uint _count) public payable {
         uint totalMinted = _tokenIds.current();
+        uint bulkPrice = PRICE;
         require(totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs!");
         require(_count > 0 && _count <= MAX_PER_MINT, "Cannot mint specified number of NFTs.");
-        require(msg.value >= PRICE.mul(_count), "Not enough ether to purchase NFTs.");
+        if (_count == 2) {
+            bulkPrice = 8750000000000000000;
+        } else if (_count == 3) {
+            bulkPrice = 7500000000000000000;
+        } else if (_count == 4) {
+            bulkPrice = 6250000000000000000;
+        } else if (_count == 5) {
+            bulkPrice = 5 ether;
+        }
+        require(msg.value >= bulkPrice.mul(_count), "Not enough matic to purchase NFTs.");
         for (uint i = 0; i < _count; i++) {
             _mintSingleNFT();
         }
--- scripts/run.js
+++ scripts/run.js
@@ -1,13 +1,13 @@
 const { utils } = require("ethers");
 
 async function main() {
-    const baseTokenURI = "ipfs://<-- IPFS HASH HERE -->/";
+    const baseTokenURI = "ipfs://QmYoWWKy7hw9TXYkb5jWtdMpKdQiV6grGjw6CFEQVwZKTR/";
 
     // Get owner/deployer's wallet address
     const [owner] = await hre.ethers.getSigners();
 
     // Get contract that we want to deploy
-    const contractFactory = await hre.ethers.getContractFactory("UnshelvedElves");
+    const contractFactory = await hre.ethers.getContractFactory("UnshelvedElvesSeries1");
 
     // Deploy contract with the correct constructor arguments
     const contract = await contractFactory.deploy(baseTokenURI);
@@ -24,12 +24,12 @@ async function main() {
     console.log("10 NFTs have been reserved");
 
     // Mint 3 NFTs by sending 0.03 ether
-    txn = await contract.mintNFTs(3, { value: utils.parseEther('0.03') });
-    await txn.wait()
+    //txn = await contract.mintNFTs(3, { value: utils.parseEther('0.03') });
+    //await txn.wait()
 
     // Get all token IDs of the owner
-    let tokens = await contract.tokensOfOwner(owner.address)
-    console.log("Owner has tokens: ", tokens);
+    //let tokens = await contract.tokensOfOwner(owner.address)
+    //console.log("Owner has tokens: ", tokens);
 
 }
 
