3474a58625be359ef1ea6c5e5dfa5b23e9d7dd04
Josef Cook <josef@assemblystudio.com>
2021-12-12 13:34:19 -0600

    Added Presale Function to contract

--- contracts/UnshelvedElvesSeries1.sol
+++ contracts/UnshelvedElvesSeries1.sol
@@ -13,12 +13,21 @@ contract UnshelvedElvesSeries1 is ERC721Enumerable, Ownable {
     uint public constant MAX_SUPPLY = 20000;
     uint public constant PRICE = 10 ether;
     uint public constant MAX_PER_MINT = 5;
-    
+    uint public PRESALE_STATUS;
     string public baseTokenURI;
 
     constructor(string memory baseURI) ERC721("Unshelved Elves (Series 1)", "UE1") {
         setBaseURI(baseURI);
         _tokenIds.increment();
+        PRESALE_STATUS = 1;
+    }
+	
+    function name() override public pure returns (string memory) {
+        return "Unshelved Elves (Series 1)";
+    }
+
+    function symbol() override public pure returns (string memory) {
+        return "UE1";
     }
 
     function reserveNFTs(uint _count) public onlyOwner {
@@ -40,18 +49,18 @@ contract UnshelvedElvesSeries1 is ERC721Enumerable, Ownable {
     function mintNFTs(uint _count) public payable {
         uint totalMinted = _tokenIds.current();
         uint bulkPrice = PRICE;
-        require(totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs!");
-        require(_count > 0 && _count <= MAX_PER_MINT, "Cannot mint specified number of NFTs.");
-        if (_count == 2) {
-            bulkPrice = 8750000000000000000;
-        } else if (_count == 3) {
-            bulkPrice = 7500000000000000000;
-        } else if (_count == 4) {
-            bulkPrice = 6250000000000000000;
-        } else if (_count == 5) {
+        require(totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs remain!");
+        require(_count > 0 && _count <= MAX_PER_MINT, "Cannot mint more than 5 NFTs at once.");
+        if (PRESALE_STATUS > 0 && _count == 2) {
+            bulkPrice = 8 ether;
+        } else if (PRESALE_STATUS > 0 && _count == 3) {
+            bulkPrice = 7 ether;
+        } else if (PRESALE_STATUS > 0 && _count == 4) {
+            bulkPrice = 6 ether;
+        } else if (PRESALE_STATUS > 0 && _count == 5) {
             bulkPrice = 5 ether;
         }
-        require(msg.value >= bulkPrice.mul(_count), "Not enough matic to purchase NFTs.");
+        require(msg.value >= bulkPrice.mul(_count), "Not enough MATIC to purchase NFTs.");
         for (uint i = 0; i < _count; i++) {
             _mintSingleNFT();
         }
@@ -73,10 +82,18 @@ contract UnshelvedElvesSeries1 is ERC721Enumerable, Ownable {
         return tokensId;
     }
 
+    function closePresale() public onlyOwner {
+        PRESALE_STATUS = 0;
+    }
+
     function withdraw() public payable onlyOwner {
         uint balance = address(this).balance;
         require(balance > 0, "No matic left to withdraw");
         (bool success, ) = (msg.sender).call{value: balance}("");
         require(success, "Transfer failed.");
+    }
+	
+    function contractURI() public pure returns (string memory) {
+        return "https://unshelvedelves.herokuapp.com/contract/ue-series-1";
     }
 }
